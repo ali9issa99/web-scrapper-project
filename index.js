770973222224bb3593e53018781ce702ccee3396
ALI9ISSA9 <ali7333170@gmail.com>
2024-07-06 07:35:59 +0300

    added the titles that I want to extract data from it

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const cheerio = require("cheerio")
-const axios = require("axios")
+const cheerio = require("cheerio");
+const axios = require("axios");
 
-console.log("hello world");
+async function performScraping() {
+  const axiosResponse = await axios.request({
+    method: "GET",
+    url: "https://www.linkedin.com/jobs/search?keywords=mlops",
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    },
+  });
+}
+
+const $ = cheerio.load(axiosResponse.data);
+const htmlElement = $(".elementClass");
+
+    const job_title = []
+    const company_name = []
+    const job_location = []
+    const job_description = []
+    const job_postdate= []
+    const skills_needed = []
+    const application_link = []
+
+
+performScraping();
